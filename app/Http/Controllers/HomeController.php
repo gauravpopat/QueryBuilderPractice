20f6d3772f9066348afb506f2e9e1014ceb4ebfc
gaurav <gauravp@zignuts.com>
2023-02-22 18:52:50 +0530

    formatting the text

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,7 +12,7 @@ class HomeController extends Controller
     public function index()
     {
         $faculty = DB::table('faculties')->get();
-        return response(view('home',['faculty'=>$faculty]));
+        return response(view('home', ['faculty' => $faculty]));
     }
 
     public function create(Request $request)
@@ -35,39 +35,39 @@ class HomeController extends Controller
             'email' => $request->email,
             'phone' => $request->phone
         ]);
-       return redirect(route('index'))->with('success','Record Inserted Successfully');
+        return redirect(route('index'))->with('success', 'Record Inserted Successfully');
     }
 
     public function editform($id)
     {
         $faculty = DB::table('faculties')->find($id);
-        return response(view('editform',['faculty'=>$faculty]));
+        return response(view('editform', ['faculty' => $faculty]));
     }
 
     public function update(Request $request, $id)
     {
         $request->validate([
-            'name' => 'required',
-            'designation' => 'required',
+            'name'          => 'required',
+            'designation'   => 'required',
             'qualification' => 'required',
-            'city' => 'required',
-            'email' => 'email | required',
-            'phone' => 'required|regex:/[7-9][0-9]{9}/',
+            'city'          => 'required',
+            'email'         => 'email | required',
+            'phone'         => 'required|regex:/[7-9][0-9]{9}/',
         ]);
-        $id = DB::table('faculties')->where('id',$id)->update([
-            'name' => $request->name,
-            'designation' => $request->designation,
-            'qualification' => $request->qualification,
-            'city' => $request->city,
-            'email' => $request->email,
-            'phone' => $request->phone
+        $id = DB::table('faculties')->where('id', $id)->update([
+            'name'            => $request->name,
+            'designation'     => $request->designation,
+            'qualification'   => $request->qualification,
+            'city'            => $request->city,
+            'email'           => $request->email,
+            'phone'           => $request->phone
         ]);
-        return redirect(route('index'))->with('success','Record Updated Successfully');
+        return redirect(route('index'))->with('success', 'Record Updated Successfully');
     }
 
     public function delete($id)
     {
-        DB::table('faculties')->where('id',$id)->delete();
-        return redirect(route('index'))->with('success','Record Deleted Successfully');
+        DB::table('faculties')->where('id', $id)->delete();
+        return redirect(route('index'))->with('success', 'Record Deleted Successfully');
     }
 }
